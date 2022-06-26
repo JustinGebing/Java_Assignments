7e4dac64779d65bdb9455f593bceb3467518008f
JustinGebing <justingebing@gmail.com>
2022-06-26 19:13:41 -0400

    Daikichi Rules

---
+++ SpringProjects/daikichi/src/main/java/com/codingdojo/daikichi/HomeController.java
@@ -0,0 +1,25 @@
+package com.codingdojo.daikichi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class HomeController {
+
+	@RequestMapping("")
+	public String welcome() {
+		return "Welcome!";
+	}
+	
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck ina ll your endeavors!";
+	}
+	
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an oppurtunity will arise, so be sure to be open to new ideas!";
+	}
+	
+}
